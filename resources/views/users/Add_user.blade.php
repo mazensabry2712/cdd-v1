ab5dd149d7b2a9778701815ce4da0f048cbb6c27
mazensabry2712 <mazensoftware30@gmail.com>
2025-05-23 01:43:55 +0300

    Update users migration to change roles_name column type to text; refactor Add_user view for improved formatting and error handling.

--- resources/views/users/Add_user.blade.php
+++ resources/views/users/Add_user.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.master')
 @section('css')
-<!-- Internal Nice-select css  -->
-<link href="{{URL::asset('assets/plugins/jquery-nice-select/css/nice-select.css')}}" rel="stylesheet" />
+    <!-- Internal Nice-select css  -->
+    <link href="{{ URL::asset('assets/plugins/jquery-nice-select/css/nice-select.css') }}" rel="stylesheet" />
 @section('title')
-Add User
+    Add User
 @stop
 
 
@@ -14,7 +14,7 @@ Add User
     <div class="my-auto">
         <div class="d-flex">
             <h4 class="content-title mb-0 my-auto">Users</h4><span class="text-muted mt-1 tx-13 mr-2 mb-0">/ Add User
-                </span>
+            </span>
         </div>
     </div>
 </div>
@@ -28,17 +28,17 @@ Add User
     <div class="col-lg-12 col-md-12">
 
         @if (count($errors) > 0)
-        <div class="alert alert-danger">
-            <button aria-label="Close" class="close" data-dismiss="alert" type="button">
-                <span aria-hidden="true">&times;</span>
-            </button>
-            <strong>Error</strong>
-            <ul>
-                @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
+            <div class="alert alert-danger">
+                <button aria-label="Close" class="close" data-dismiss="alert" type="button">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+                <strong>Error</strong>
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
         @endif
 
         <div class="card">
@@ -49,8 +49,8 @@ Add User
                     </div>
                 </div><br>
                 <form class="parsley-style-1" id="selectForm2" autocomplete="off" name="selectForm2"
-                    action="{{route('users.store','test')}}" method="post">
-                    {{csrf_field()}}
+                    action="{{ route('users.store', 'test') }}" method="post">
+                    {{ csrf_field() }}
 
                     <div class="">
 
@@ -58,13 +58,15 @@ Add User
                             <div class="parsley-input col-md-6" id="fnWrapper">
                                 <label> User Name: <span class="tx-danger">*</span></label>
                                 <input class="form-control form-control-sm mg-b-20"
-                                    data-parsley-class-handler="#lnWrapper" name="name" required="" type="text">
+                                    data-parsley-class-handler="#lnWrapper" name="name" required=""
+                                    type="text">
                             </div>
 
                             <div class="parsley-input col-md-6 mg-t-20 mg-md-t-0" id="lnWrapper">
                                 <label>Email: <span class="tx-danger">*</span></label>
                                 <input class="form-control form-control-sm mg-b-20"
-                                    data-parsley-class-handler="#lnWrapper" name="email" required="" type="email">
+                                    data-parsley-class-handler="#lnWrapper" name="email" required=""
+                                    type="email">
                             </div>
                         </div>
 
@@ -93,12 +95,24 @@ Add User
                             </select>
                         </div>
                     </div>
-<div class="row mg-b-20">
+
+                    <div class="row mg-b-20">
+                        <div class="col-xs-12 col-md-12">
+                            <div class="form-group">
+                                <label class="form-label"> User Permission</label>
+                                {!! Form::select('roles_name[]', $roles, [], ['class' => 'form-control', 'multiple']) !!}
+                            </div>
+                        </div>
+                    </div>
+
+
+
+                    {{-- <div class="row mg-b-20">
     <div class="col-xs-12 col-md-12">
         <div class="form-group">
             <label class="form-label">User Permission</label>
             <select name="roles_name[]" class="form-control" multiple>
-                @foreach($roles as $value => $label)
+                @foreach ($roles as $value => $label)
                     <option value="{{ $value }}"
                         {{ in_array($value, old('roles_name', [])) ? 'selected' : '' }}>
                         {{ $label }}
@@ -107,16 +121,9 @@ Add User
             </select>
         </div>
     </div>
-</div>
+</div> --}}
 
-                    {{-- <div class="row mg-b-20">
-                        <div class="col-xs-12 col-md-12">
-                            <div class="form-group">
-                                <label class="form-label"> User Permission</label>
-                                {!! Form::select('roles_name[]', $roles,[], array('class' => 'form-control','multiple')) !!}
-                            </div>
-                        </div>
-                    </div> --}}
+
                     <div class="col-xs-12 col-sm-12 col-md-12 text-center">
                         <button class="btn btn-main-primary pd-x-20" type="submit">Save</button>
                     </div>
@@ -135,11 +142,11 @@ Add User
 
 
 <!-- Internal Nice-select js-->
-<script src="{{URL::asset('assets/plugins/jquery-nice-select/js/jquery.nice-select.js')}}"></script>
-<script src="{{URL::asset('assets/plugins/jquery-nice-select/js/nice-select.js')}}"></script>
+<script src="{{ URL::asset('assets/plugins/jquery-nice-select/js/jquery.nice-select.js') }}"></script>
+<script src="{{ URL::asset('assets/plugins/jquery-nice-select/js/nice-select.js') }}"></script>
 
 <!--Internal  Parsley.min js -->
-<script src="{{URL::asset('assets/plugins/parsleyjs/parsley.min.js')}}"></script>
+<script src="{{ URL::asset('assets/plugins/parsleyjs/parsley.min.js') }}"></script>
 <!-- Internal Form-validation js -->
-<script src="{{URL::asset('assets/js/form-validation.js')}}"></script>
+<script src="{{ URL::asset('assets/js/form-validation.js') }}"></script>
 @endsection
